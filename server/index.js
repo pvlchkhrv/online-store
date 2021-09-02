1ec7fa8e86ce996b0d8b95d3bff3f1197a3ef263
pvl <pvlchkhrv@gmail.com>
2021-09-02 17:47:10 +0300

    feat: add CRUD to brand, type & device controllers

--- server/index.js
+++ server/index.js
@@ -3,21 +3,29 @@ const express = require('express');
 const sequelize = require('./db');
 const models = require('./models/models');
 const cors = require('cors');
+const fileUpload = require('express-fileupload'); 
 const router = require('./routes/index');
+const errorHandler = require('./middleware/ErrorHandlingMiddleWare');
+
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload({}));
 app.use('/api', router);
+
+// should be the last middleware
+app.use(errorHandler);
+
 const start = async () => {
   try {
     await sequelize.authenticate(); //устанавливает подклчение к базе данных
     await sequelize.sync();
     app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 }
 
