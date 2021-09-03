7717502164bcc368f7e83e4f1ec66e6067579a1b
pvl <pvlchkhrv@gmail.com>
2021-09-03 17:57:33 +0300

    feat: add device getting realization

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,4 +1,4 @@
-const { Device } = require('../models/models');
+const { Device, DeviceInfo } = require('../models/models');
 const ApiError = require('../error/ApiError');
 const uuid = require('uuid');
 const path = require('path');
@@ -6,12 +6,23 @@ const path = require('path');
 class DeviceController {
   async create(req, res, next) {
     try {
-      const { name, price, brandId, typeId, info} = req.body;
+      const { name, price, brandId, typeId, info } = req.body;
       const { img } = req.files;
       let fileName = uuid.v4() + '.jpg';
       img.mv(path.resolve(__dirname, '..', 'static', fileName));
-
       const device = await Device.create({ name, price, brandId, typeId, img: fileName });
+
+      if (info) {
+        info = JSON.parse(info);
+        info.ferEach( i => 
+          DeviceInfo.create({
+            title: i.title,
+            deviceId: i.deviceId, 
+            descripttion: i.descripttion,
+          }))
+      }
+
+
 
       return res.json(device);
     } catch (e) {
@@ -20,11 +31,36 @@ class DeviceController {
   }
 
   async getAll(req, res) {
-
+    const {brandId, typeId, limit, page} = req.body;
+    page = page || 1;
+    limit = limit || 9;
+    let offset = page * limit - limit;
+    let devices;
+    if (!brandId && !typeId) {
+      devices = await Device.findCountAll(limit, offset);
+    }
+    if (brandId && !typeId) {
+      devices = await Device.findCountAll({where: {brandId}});
+    }
+    if (!brandId && typeId) {
+      devices = await Device.findCountAll({where: {typeId}});
+    }
+    if (brandId && typeId) {
+      devices = await Device.findCountAll({where: {brandId, typeId}}); 
+    }
+    return res.json(devices);
   }
 
   async getById(req, res) {
+    const {id} = req.params;
+    const device = await Device.findOne(
+      {
+        where: {id},
+        include: [{model: DeviceInfo, as: 'info'}]
+      },
 
+    )
+    return res.json(device);
   }
 }
 
