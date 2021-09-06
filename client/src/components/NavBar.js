3ef60d370767f80ad7b223bd75bbdf7d7493790d
pvl <pvlchkhrv@gmail.com>
2021-09-06 13:57:58 +0300

    feat: add device page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react';
 import { Context } from '../index';
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import { SHOP_ROUTE } from '../utils/consts';
-import { NavLink } from 'react-router-dom'
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from '../utils/consts';
+import { NavLink, useHistory} from 'react-router-dom'
 import Button from '@restart/ui/esm/Button';
 import {observer} from 'mobx-react-lite';
 
 const NavBar = observer(() => {
   const { user } = useContext(Context);
+  const history = useHistory();
 
   return (
     <Navbar bg="dark" variant="dark">
@@ -16,8 +17,8 @@ const NavBar = observer(() => {
         {
           user.isAuth
             ? <Nav className="ml-auto" style={{ color: 'white' }}>
-              <Button variant='outline-light'>Admin panel</Button>
-              <Button variant='outline-light' onClick={() => user.setIsAuth(false)} className='ml-4'>Logout</Button>
+              <Button variant='outline-light' onClick={() => history.push(ADMIN_ROUTE)}>Admin panel</Button>
+              <Button variant='outline-light' onClick={() => history.push(LOGIN_ROUTE)} className='ml-4'>Logout</Button>
             </Nav>
             : <Nav className="ml-auto" style={{ color: 'white' }}>
               <Button variant='outline-light' onClick={() => user.setIsAuth(true)}>Authorization</Button>
--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,10 +1,38 @@
+import Button from '@restart/ui/esm/Button';
 import React from 'react';
+import { Card, Col, Container, Image, Row } from 'react-bootstrap';
+import bigStar from '../assets/Star_big.png';
 
 const DevicePage = () => {
+  const device = { id: 1, name: '12 pro', price: 1200, rating: 5, img: 'https://images.samsung.com/uk/smartphones/galaxy-s21/buy/02_ImageCarousel/01_FamilyShot/S21_Family_KV_PC_img.jpg?imwidth=2560' }
   return (
-    <div>
-      DEVICE
-    </div>
+    <Container className='mt-3'>
+      <Row>
+        <Col md={4}>
+          <Image width={300} height={300} src={device.img} />
+        </Col>
+        <Col md={4}>
+          <Row className='d-flex flex-column align-items-center'>
+            <h2>{device.name}</h2>
+            <div
+              className='d-flex align-items-center justify-content-center'
+              style={{ background: `url(${bigStar}) no-repeat center center`, width: 240, height: 240, backgroundSize: 'cover', fontSize: 64 }}
+            >
+              {device.rating}
+            </div>
+          </Row>
+        </Col>
+        <Col md={4}>
+          <Card
+            className='d-flex flex-xolumn align-items-center justify-content-around'
+            style={{ width: 300, height: 300, fontSize: 32, border: '5px solid lightgray' }}
+          >
+            <h3>From {device.price} $</h3>
+            <Button variant='outline-dark'>Add to basket</Button>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
