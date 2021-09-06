a0edde97d705ad62f29f6c1322b2d214e3971708
pvl <pvlchkhrv@gmail.com>
2021-09-06 11:38:18 +0300

    feat: add auth

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import { Card, Container, Form, Button, Row } from 'react-bootstrap';
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts';
+import { NavLink, useLocation } from 'react-router-dom';
+
 
 const Auth = () => {
+  const location = useLocation();
+  const isLogin = location.pathname === LOGIN_ROUTE;
+  console.log(location);
   return (
-    <div>
-      AUTH
-    </div>
+    <Container
+      className='d-flex justify-content-center align-items-center'
+      style={{ height: window.innerHeight - 54 }}
+    >
+      <Card style={{ width: 600 }} className='p-5'>
+        <h2 className='m-auto'>{isLogin ? 'Authorization' : 'Registration'}</h2>
+        <Form className='d-flex flex-column'>
+          <Form.Control
+            className='mt-3'
+            placeholder='Enter you email...'
+          />
+          <Form.Control
+            className='mt-3'
+            placeholder='Enter you password...'
+          />
+          <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
+            {
+              isLogin
+                ? <div>No account yet? <NavLink to={REGISTRATION_ROUTE}>Register</NavLink></div>
+                : <div>Go to <NavLink to={LOGIN_ROUTE}>Login</NavLink></div>
+
+            }
+
+            <Button variant='outline-success'>
+              {
+                isLogin ? 'Login' : 'Register'
+              }
+            </Button>
+          </Row>
+        </Form>
+      </Card>
+
+    </Container>
   );
 }
 
--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Shop = () => {
   console.log('SHOP COMPONENT');
   return (
-    <div>
-      SHOP
-    </div>
+    <Container>
+      <Row>
+        <Col md={3}>
+
+        </Col>
+        <Col md={9}>
+
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
